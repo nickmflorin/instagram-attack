39ba86be60a2970c9f02b0c301ddd7e9719e0b70
Nicholas Florin <nflorin@theatlantic.com>
2019-03-24 20:55:55 -0400

    Copy over

---
+++ lib/session.py
@@ -0,0 +1,58 @@
+# Date: 05/05/2018
+# Author: Mohamed
+# Description: Session Handler
+
+from os import remove
+from sys import version_info
+from os.path import exists as path 
+from csv import DictWriter, DictReader
+
+if version_info[0] == 2:
+    from io import open
+
+class Session(object):
+
+    is_busy = False
+
+    def __init__(self, username, passlist):
+        self.file = '.{}_{}.csv'.format(username, passlist.replace('\\', '_').replace('/', '_'))
+
+    @property 
+    def exists(self):
+        return path(self.file)
+
+    def read(self):
+        with open(self.file, 'rt', encoding='utf-8') as csvfile:
+            session = DictReader(csvfile, delimiter = ',')
+            try:return [_ for _ in session][0]
+            except:pass 
+    
+    def _write(self, attempts, _list):
+        with open(self.file, 'wt', encoding='utf-8') as csvfile:
+            fieldnames = ['attempts', 'list']
+            writer = DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({ 'attempts': attempts, 'list': _list })
+        
+    def write(self, attempts, _list):
+        if not attempts:
+            return
+        
+        while Session.is_busy:
+            pass 
+        
+        try:
+            Session.is_busy = True
+            self._write(attempts, _list)
+        except:
+            pass 
+        finally:
+            Session.is_busy = False        
+        
+    def delete(self):
+        if self.exists:
+            try:
+                remove(self.file) 
+            except:
+                pass 
