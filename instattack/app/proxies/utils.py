662425dc6d4929addecd871bf4a82a67cf311352
nickmflorin <nickmflorin@gmail.com>
2019-06-06 01:31:27 -0400

    New scraping style

--- instattack/app/proxies/utils.py
+++ instattack/app/proxies/utils.py
@@ -1,22 +1,75 @@
 from bs4 import BeautifulSoup
 import requests
 
+from instattack.lib.utils import start_and_stop
 
-def scrape_proxies(limit=None):
-    """
-    Not currently used, but could be useful.
-    """
+
+class ProxyScrape(object):
+
+    def __init__(self, limit=None):
+        self.limit = limit
+        self.proxies = []
+
+    def __call__(self):
+        with start_and_stop(f"Scraping Proxies from {self.url}"):
+            r = requests.get(self.url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            self.eat_soup(soup)
+            return self.proxies
+
+    def add(self, host=None, port=None):
+        print('Host : %s' % host)
+        self.proxies.append({
+            'host': host,
+            'port': port
+        })
+
+    def eat_rows(self, rows):
+        row_index = 0
+        while not self.stop and row_index < len(rows):
+            self.eat_row(rows[row_index])
+            row_index += 1
+
+    @property
+    def stop(self):
+        if self.limit and len(self.proxies) == self.limit:
+            return True
+        return False
+
+
+class GatherProxyOrg(ProxyScrape):
+    url = "http://http://www.gatherproxy.com//"
+
+    def eat_soup(self, soup):
+        table = soup.find_all('table', {'id': 'tblproxy'})[0]
+        rows = table.findChildren('tr')[2:]
+        self.eat_rows(rows)
+
+    def eat_row(self, row):
+        if self.limit and len(self.proxies) == self.limit:
+            return None
+
+        children = row.findChildren('td')
+        try:
+            host = children[1].text
+            port = children[2].text
+        except IndexError:
+            pass
+        else:
+            self.add(host=host, port=port)
+
+
+class USProxyOrg(ProxyScrape):
     url = "https://www.us-proxy.org/"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
 
-    proxies = []
-    table = soup.find_all('table', {'id': 'proxylisttable'})[0]
-    rows = table.findChildren('tr')[1:]
+    def eat_soup(self, soup):
+        table = soup.find_all('table', {'id': 'proxylisttable'})[0]
+        rows = table.findChildren('tr')[1:]
+        self.eat_rows(rows)
 
-    for row in rows:
-        if limit and len(proxies) == limit:
-            break
+    def eat_row(self, row):
+        if self.limit and len(self.proxies) == self.limit:
+            return None
 
         children = row.findChildren('td')
         try:
@@ -24,12 +77,19 @@ def scrape_proxies(limit=None):
             port = children[1].text
             is_https = children[6].text
         except IndexError:
-            break
+            pass
         else:
             # We only want HTTP proxies.
             if is_https == 'no':
-                proxies.append({
-                    'host': host,
-                    'port': port
-                })
-    return proxies
+                self.add(host=host, port=port)
+
+
+def scrape_proxies(limit=None):
+    """
+    Not currently used, but could be useful.
+    """
+    # scrape = USProxyOrg(limit=limit)
+    scrape = GatherProxyOrg(limit=limit)
+    scrape()
+    return []
+    # return scrape()
