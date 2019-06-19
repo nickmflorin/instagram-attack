8809c776b925e57997dbcb832868d7b7ea4c2ee7
nickmflorin <nickmflorin@gmail.com>
2019-06-19 02:57:42 -0400

    Grid structure close to working

---
+++ instattack/lib/diagnostics/cells.py
@@ -0,0 +1,608 @@
+from dataclasses import dataclass, InitVar, field, asdict, fields
+import math
+import typing
+
+
+def simple_descriptor(obj_class):
+
+    class Descriptor(object):
+
+        def __get__(self, obj, objtype):
+            return obj_class
+
+    return Descriptor()
+
+
+def raise_invalid_value(attr):
+    """
+    [x] TODO:
+    --------
+    This should be more of a runtime error, but we might want to raise this
+    as an exception built into the app if we ever exposure this as an API.
+    """
+    raise ValueError('Invalid value specified for %s.' % attr)
+
+
+def round_value(value):
+
+    decimal = float(value) % 1
+    if decimal != 0.0:
+        if decimal < 0.5:
+            return int(value)
+        else:
+            return math.ceil(value)
+    else:
+        return int(value)
+
+
+@dataclass
+class IntegeredMixin:
+
+    all: InitVar[int] = None
+
+    @classmethod
+    def none(cls):
+        """
+        Verbose method for returning an instance of the dataclass with all
+        0 values.
+        """
+        return cls()
+
+    @classmethod
+    def zero(cls):
+        return cls()
+
+    @classmethod
+    def constant(cls, val):
+        """
+        Verbose method for returning an instance of the dataclass with all values
+        set to a constant.
+        """
+        return Padding(all=val)
+
+    def __post_init__(self, all):
+        """
+        IntegeredMixin is primarily used for the __post_init__ functionality,
+        which does the following:
+
+            (1) Force type cases to integers for safety, raising exceptions where
+            invalid values supplied.
+            (2) Allows `all` to be specified as a parameter which sets all values
+                to the constant value specified by `all`.
+
+        For curses, all dimensions, coordinates and distances need to be specified
+        in terms of the number of columns or rows, which requires integer
+        coercion.
+
+        To be safe, for objects with all integer properties that default to 0
+        (which will be common for this) this mixin will force the values to be
+        integers on initialization so we don't have to always explicitly typecast.
+        """
+        if all is not None:
+            for fld in fields(self):
+                if fld.init:
+                    setattr(self, fld.name, all)
+
+        for fld in fields(self):
+            val = getattr(self, fld.name)
+            if val is None:
+                raise_invalid_value(fld.name)
+
+            try:
+                # val = round_value(val)
+                val = round_value(val)
+            except ValueError:
+                raise_invalid_value(fld.name)
+            else:
+                setattr(self, fld.name, val)
+
+
+@dataclass
+class Dimensions(IntegeredMixin):
+
+    height: int = 0
+    width: int = 0
+
+
+@dataclass
+class Coordinates(IntegeredMixin):
+
+    x1: int = 0
+    y1: int = 0
+    x2: int = 0
+    y2: int = 0
+
+
+@dataclass
+class Padding(IntegeredMixin):
+
+    left: int = 0
+    right: int = 0
+    top: int = 0
+    bottom: int = 0
+
+    v: InitVar[int] = None
+    h: InitVar[int] = None
+
+    def __post_init__(self, all, v, h):
+        if v:
+            self.top = self.bottom = v
+        if h:
+            self.left = self.right = h
+        super(Padding, self).__post_init__(all)
+
+
+@dataclass
+class Cell:
+    """
+    [x] Note: Should be `Columns` or `Rows`, but since those are not defined
+    yet we cannot reference those types directly.
+    """
+    padding: Padding = Padding.none()
+    coordinates: Coordinates = Coordinates.none()
+    absolute_coordinates: Coordinates = Coordinates.none()
+    dimensions: Dimensions = Dimensions.none()
+
+    def _adopt_children(self):
+
+        def get_siblings(child):
+            return [
+                other_child for other_child in self.children
+                if other_child != child
+            ]
+
+        children = self.children()
+
+        [setattr(child, 'parent', self) for child in children]
+        [setattr(child, 'index', i) for i, child in enumerate(children)]
+
+        for child in children:
+            siblings = children[:]
+            siblings = [ch for ch in siblings if ch.index != child.index]
+            setattr(child, 'siblings', siblings)
+
+    def measure(self):
+        self.dimensions = Dimensions(width=self.width, height=self.height)
+
+        children = self.children()
+        for child in children:
+            child.measure()
+
+    @property
+    def __dict__(self):
+        return {
+            'padding': self.padding.__dict__,
+            'coordinates': self.coordinates.__dict__,
+            'dimensions': self.dimensions.__dict__,
+        }
+
+    def children_windows(self, window, border=None):
+        windows = []
+        children = self.children()
+        if children:
+            for child in children:
+                win = child.draw(window, border=border)
+                # win.refresh()
+                windows.append(win)
+        return windows
+
+    def draw(self, window, border=None):
+        windows = []
+
+        base_window = window.subwin(
+            self.dimensions.height,
+            self.dimensions.width,
+            self.coordinates.y1,
+            self.coordinates.x1
+        )
+        base_window.addstr(2, 2, f"{self.__class__.__name__}: {self.index}")
+
+        if border:
+            base_window.attron(border)
+            base_window.box()
+            base_window.attroff(border)
+
+        base_window.refresh()
+
+        windows = [base_window]
+        windows += [self.children_windows(base_window, border=border)]
+        return windows
+
+
+@dataclass
+class Column(Cell):
+    """
+    [x] Note: Should be `Columns` or `Rows`, but since those are not defined
+    yet we cannot reference those types directly.
+    """
+    rw: InitVar[int] = 1
+    rows: typing.Optional[typing.List[typing.Any]] = field(default_factory=list)
+    siblings: typing.Optional[typing.List[typing.Any]] = field(default_factory=list)
+
+    parent: typing.Any = field(default=None, repr=False)
+    index: int = field(init=False, repr=False)
+
+    def __post_init__(self, rw):
+        self._rw = rw
+        self._adopt_children()
+
+    @property
+    def _width_ratio(self):
+        """
+        When specifying the children of a specific Row or Column, we specify
+        an integer number that reflects the relative height or width compared
+        to the siblings.
+
+        Ex:
+            >>> [sibling._rw for sibling in siblings]
+            >>> [1, 2, 5]
+
+        Ratios: [0.125, 0.25, 0.375]
+        """
+        """
+        [x] NOTE:
+        -------
+        There are a lot of problems with roundign to integers and the wide spaces
+        used as intervals (columns/rows).  A rounded value can very easily cause
+        the border to extend past the screen a tiny bit which makes curses complain.
+
+        What we should do is find an optimal width and height based on the screen
+        height and width that is less than the total area of the screen.  A height
+        and width that is divisble by some interval, maybe evenly divisible by
+        the total _rh and _rw value.  Then, we will get better results, with
+        extra spacing in the window that we don't need to worry about.
+        """
+        raise Exception('Above Note')
+        width_ratio = self._rw / (self._rw + sum([sib._rw for sib in self.siblings]))
+        return width_ratio
+        # We are going to do more rounding earlier rather than later to minimize
+        # the offsetting effect of large increments (rows/columns) being used
+        # instead of something like pixels.
+        width_ratio = 100.0 * width_ratio
+        width_ratio = round_value(width_ratio)
+        return width_ratio / 100.0
+
+    def _measure(self):
+        """
+        [x] TODO:
+        --------
+        Have properties for coordinates that are both relative to parent and
+        relative to the top level grid.
+
+        [x] TODO:
+        --------
+        We have to add a spacing property to parents to allow them to space
+        out the children more easily.  Maybe even margins instead?
+
+        [!] Temporarily using spacing of 2.0
+        """
+        SPACING = 1  # Hard Code For Now
+
+        total_spacing = SPACING * (len(self.siblings) - 1)
+
+        available_width = (
+            self.parent.dimensions.width
+            - self.parent.padding.left
+            - self.parent.padding.right
+            - total_spacing)
+
+        available_height = (
+            self.parent.dimensions.height
+            - self.parent.padding.top
+            - self.parent.padding.bottom)
+
+        self.dimensions = Dimensions(
+            width=self._width_ratio * available_width - 1.0,
+            height=available_height,
+        )
+
+        initial_x = self.parent.padding.left + self.parent.coordinates.x1
+        initial_y = self.parent.padding.top + self.parent.coordinates.y1
+
+        if self.index != 0:
+
+            # REVIEW
+            # THIS IS GOING TO CAUSE PROBLEMS (I THINK)
+            # THINK ABOUT NESTED NESTED CHILDREN AND SPACING OF PARENT ELEMENTS
+            cum_sibling_width = sum([sibling.dimensions.width
+                for sibling in self.siblings[:self.index]])
+
+            initial_x = self.siblings[self.index - 1].coordinates.x1
+            initial_x += self.siblings[self.index - 1].dimensions.width
+            initial_x += SPACING
+            """
+            [x] Review: Not sure why this is having an effect, but we get better
+            results when doing:
+            >>> cum_spacing = spacing
+                    vs.
+            >>> cum_spacing = spacing * self.index
+
+            [x] Review: Not sure why this is having an effect, but we get better
+            results when doing:
+            >>> initial_y += cum_spacing + cum_sibling_height - self.parent.padding.left
+                    vs.
+            >>> initial_y += cum_spacing + cum_sibling_height
+
+            Subtracting the padding at the left doesn't make a lot of sense.
+            """
+            # cum_spacing = SPACING
+            # initial_x += cum_spacing + cum_sibling_width
+
+        self.coordinates = Coordinates(
+            x1=initial_x,
+            x2=initial_x + available_width,
+            y1=initial_y,
+            y2=initial_y + available_height,
+        )
+
+        self._measure_children()
+
+    def _measure_children(self):
+        children = self.children()
+        for child in children:
+            child._measure()
+
+    def children(self):
+        return self.rows
+
+    @property
+    def __dict__(self):
+        data = super(Column, self).__dict__
+        children = self.children()
+        data.update(
+            rows=[row.__dict__ for row in children]
+        )
+        return data
+
+
+@dataclass
+class Row(Cell):
+    """
+    [x] Note: Should be `Columns` or `Rows`, but since those are not defined
+    yet we cannot reference those types directly.
+    """
+    rh: InitVar[int] = 1
+    columns: typing.Optional[typing.List[typing.Any]] = field(default_factory=list)
+    siblings: typing.Optional[typing.List[typing.Any]] = field(default_factory=list)
+
+    parent: typing.Any = field(default=None, repr=False)
+    index: int = field(init=False, repr=False)
+
+    def __post_init__(self, rh):
+        self._rh = rh
+        self._adopt_children()
+
+    @property
+    def _height_ratio(self):
+        """
+        When specifying the children of a specific Row or Column, we specify
+        an integer number that reflects the relative height or width compared
+        to the siblings.
+
+        Ex:
+            >>> [sibling._rh for sibling in siblings]
+            >>> [1, 2, 5]
+
+        Ratios: [0.125, 0.25, 0.375]
+        """
+        height_ratio = self._rh / (self._rh + sum([sib._rh for sib in self.siblings]))
+
+        # We are going to do more rounding earlier rather than later to minimize
+        # the offsetting effect of large increments (rows/columns) being used
+        # instead of something like pixels.
+        height_ratio = 100.0 * height_ratio
+        height_ratio = round_value(height_ratio)
+        return height_ratio / 100.0
+
+    def _measure(self):
+        """
+        [x] TODO:
+        --------
+        Have properties for coordinates that are both relative to parent and
+        relative to the top level grid.
+
+        [x] TODO:
+        --------
+        We have to add a spacing property to parents to allow them to space
+        out the children more easily.  Maybe even margins instead?
+
+        [!] Temporarily using spacing of 2.0
+        """
+        SPACING = 1  # Hard Code For Now
+
+        total_spacing = SPACING * (len(self.siblings) - 1)
+
+        available_width = round_value(
+            self.parent.dimensions.width
+            - self.parent.padding.left
+            - self.parent.padding.right)
+
+        available_height = round_value(
+            self.parent.dimensions.height
+            - self.parent.padding.top
+            - self.parent.padding.bottom
+            - total_spacing)
+
+        self.dimensions = Dimensions(
+            width=available_width,
+            height=self._height_ratio * available_height
+        )
+
+        initial_x = self.parent.padding.left + self.parent.coordinates.x1
+        initial_y = self.parent.padding.top + self.parent.coordinates.y1
+
+        if self.index != 0:
+            cum_sibling_height = sum([sibling.dimensions.height
+                for sibling in self.siblings[:self.index]])
+
+            """
+            [x] Review: Not sure why this is having an effect, but we get better
+            results when doing:
+            >>> cum_spacing = spacing
+                    vs.
+            >>> cum_spacing = spacing * self.index
+
+            [x] Review: Not sure why this is having an effect, but we get better
+            results when doing:
+            >>> initial_y += cum_spacing + cum_sibling_height - self.parent.padding.top
+                    vs.
+            >>> initial_y += cum_spacing + cum_sibling_height
+
+            Subtracting the padding at the top doesn't make a lot of sense.
+            """
+            cum_spacing = SPACING
+            initial_y += cum_spacing + cum_sibling_height - self.parent.padding.top
+
+        self.coordinates = Coordinates(
+            x1=initial_x,
+            x2=initial_x + available_width,
+            y1=initial_y,
+            y2=initial_y + available_height,
+        )
+
+        self._measure_children()
+
+    def _measure_children(self):
+        children = self.children()
+        for child in children:
+            child._measure()
+
+    def children(self):
+        return self.columns
+
+    @property
+    def __dict__(self):
+        data = super(Row, self).__dict__
+        children = self.children()
+        data.update(
+            columns=[col.__dict__ for col in children]
+        )
+        return data
+
+
+@dataclass
+class Grid(Cell):
+    """
+    [x] TODO:
+    --------
+    Right now, we are thinking in terms of a traditional HTML table
+    where the <tr> elements contain the <td> elements (so the top level
+    Cell contains rows, each of which contains columns).
+
+    We should make this more flexible in the future.
+    """
+    height: InitVar[int] = 0
+    width: InitVar[int] = 0
+    rows: typing.List[Row] = field(default_factory=list)
+
+    # We use descriptors for these so we can do things like:
+    # >>> Grid.Padding.constant(2)
+    # This will hopefully make for a smoother API if we ever want to treat it
+    # that way.
+    Padding = simple_descriptor(Padding)
+    Dimensions = simple_descriptor(Dimensions)
+    Row = simple_descriptor(Row)
+    Column = simple_descriptor(Column)
+
+    def __post_init__(self, height, width):
+        """
+        [x] NOTE:
+        --------
+        We cannot perform any measurement methods on the individual Row and Cell
+        objects in __post_init__ because the recursion has to work back up the
+        tree to the top level grid where those elements are assigned the Grid
+        as their parent.
+
+        The Grid object's post_init is called after all of the Row and Column
+        post inits.
+
+        This means that the Grid is necessary to contain Row and Column objects
+        for now, because it has the top level height and width, unless we start
+        allowing things like:
+
+        row = Row(
+            height=100,
+            width=100,
+            children=[...]
+        )
+
+        In essence, __post_init__ works at the bottom of the tree up towards
+        the top (Grid):
+
+        __post_init__:
+
+        column, column, column, column, (Each Row Broken in 2 Columns)
+        row, row (Grid Broken in 2 Rows)
+        Grid
+
+        Then, we work from the top back down to calculate measurements:
+
+        dimensions:
+
+        Grid -> Measure Rows
+            row -> Measure Cols
+                column
+                column
+            row -> Measure Cols
+                column
+                column
+        """
+        self.dimensions = Dimensions(height=height, width=width)
+        self._adopt_children()
+        self._measure()
+
+    def _measure(self):
+        self.coordinates = Coordinates.none()
+        # self.absolute_coordinates = Coordinates.none()
+        self._measure_children()
+
+    def _measure_children(self):
+        children = self.children()
+        for child in children:
+            child._measure()
+
+    def children(self):
+        return self.rows
+
+    @property
+    def __dict__(self):
+        data = super(Grid, self).__dict__
+        data.update(
+            dimensions=self.dimensions.__dict__,
+            rows=[row.__dict__ for row in self.rows]
+        )
+        return data
+
+    @classmethod
+    def from_screen(cls, screen, **kwargs):
+        height, width = screen.getmaxyx()
+        return cls(height=height, width=width, **kwargs)
+
+    def children_windows(self, window, border=None):
+        windows = []
+        for child in self.children():
+            win = child.draw(window, border=border)
+            # win.refresh()
+            windows.append(win)
+        return windows
+
+    def draw(self, window, border=None):
+        windows = []
+
+        base_window = window.subwin(
+            self.dimensions.height,
+            self.dimensions.width,
+            self.coordinates.y1,
+            self.coordinates.x1
+        )
+
+        if border:
+            base_window.attron(border)
+            base_window.box()
+            base_window.attroff(border)
+
+        base_window.refresh()
+
+        windows = [base_window]
+        windows += [self.children_windows(base_window, border=border)]
+        return windows
--- instattack/lib/diagnostics/main.py
+++ instattack/lib/diagnostics/main.py
@@ -6,6 +6,7 @@ from .colors import colors
 from .pairs import Pairs
 from .blueprint import BluePrint
 from .dialogs import AnalyticsDialog, LogDialog, DebugLogDialog
+from .cells import Grid
 
 
 diagnostics = signal('diagnostics')
@@ -36,6 +37,7 @@ global screen
 class Diagnostics(object):
 
     def __init__(self, stdscreen, on_exit, stop_event):
+        self.screen = stdscreen
 
         self.pairs = Pairs()
         self.blueprints = {}
@@ -43,37 +45,60 @@ class Diagnostics(object):
         self.exit = on_exit
         self.stop_event = stop_event
 
-        # Here, w is a ratio of the leftover space, but we want to eventually
-        # allow to specify leftover space OR total horizontal space.
-        self.blueprints['analytics'] = BluePrint(w=0.5, h=0.5, parent=screen, x=1, y=1)
-        self.blueprints['logging'] = BluePrint(w=0.5, h=0.5, parent=screen, x=self.blueprints['analytics'].x2 + 1, y=1)
-        self.blueprints['debug'] = BluePrint(w=1.0, h=0.5, parent=screen, x=1, y=self.blueprints['logging'].y2 + 1)
+        # Grid Not Working Perfectly, But Works for Our Purposes Now
+        self.grid = Grid.from_screen(
+            stdscreen,
+            padding=Grid.Padding(h=1, v=1),
+            rows=[
+                Grid.Row(
+                    rh=2,
+                    columns=[
+                        Grid.Column(rw=1),
+                        Grid.Column(rw=1),
+                    ]
+                ),
+                Grid.Row(
+                    rh=4,
+                ),
+                Grid.Row(
+                    rh=4,
+                )
+            ]
+        )
 
-        self.dialogs = {
-            'analytics': AnalyticsDialog(
-                blueprint=self.blueprints['analytics'],
-                on_exit=self.exit,
-                stop_event=self.stop_event,
-                border=self.pairs[colors.black, colors.transparent]
-            ),
-            'logging': LogDialog(
-                blueprint=self.blueprints['logging'],
-                on_exit=self.exit,
-                stop_event=self.stop_event,
-                border=self.pairs[colors.black, colors.transparent]
-            ),
-            'debug': DebugLogDialog(
-                blueprint=self.blueprints['debug'],
-                on_exit=self.exit,
-                stop_event=self.stop_event,
-                border=self.pairs[colors.black, colors.transparent]
-            )
-        }
+        # # Here, w is a ratio of the leftover space, but we want to eventually
+        # # allow to specify leftover space OR total horizontal space.
+        # self.blueprints['analytics'] = BluePrint(w=0.5, h=0.5, parent=screen, x=1, y=1)
+        # self.blueprints['logging'] = BluePrint(w=0.5, h=0.5, parent=screen, x=self.blueprints['analytics'].x2 + 1, y=1)
+        # self.blueprints['debug'] = BluePrint(w=1.0, h=0.5, parent=screen, x=1, y=self.blueprints['logging'].y2 + 1)
+
+        # self.dialogs = {
+        #     'analytics': AnalyticsDialog(
+        #         blueprint=self.blueprints['analytics'],
+        #         on_exit=self.exit,
+        #         stop_event=self.stop_event,
+        #         border=self.pairs[colors.black, colors.transparent]
+        #     ),
+        #     'logging': LogDialog(
+        #         blueprint=self.blueprints['logging'],
+        #         on_exit=self.exit,
+        #         stop_event=self.stop_event,
+        #         border=self.pairs[colors.black, colors.transparent]
+        #     ),
+        #     'debug': DebugLogDialog(
+        #         blueprint=self.blueprints['debug'],
+        #         on_exit=self.exit,
+        #         stop_event=self.stop_event,
+        #         border=self.pairs[colors.black, colors.transparent]
+        #     )
+        # }
 
     def draw(self):
-        self.dialogs['analytics'].draw()
-        self.dialogs['logging'].draw()
-        self.dialogs['debug'].draw()
+        self.grid.draw(self.screen, border=self.pairs[colors.black, colors.transparent])
+
+        # self.dialogs['analytics'].draw()
+        # self.dialogs['logging'].draw()
+        # self.dialogs['debug'].draw()
 
         global screen
         screen.refresh()
@@ -83,12 +108,14 @@ class Diagnostics(object):
         if not dialog:
             self.exit()
             raise RuntimeError('Dialog Must be Provided')
-        self.dialogs[dialog].dispatch(event)
+        # print('Got Event')
+        # self.dialogs[dialog].dispatch(event)
 
     def start(self):
-        thread = threading.Thread(target=self.dialogs['logging'].start)
-        thread.start()
-        thread.join()
+        # thread = threading.Thread(target=self.dialogs['logging'].start)
+        # thread.start()
+        # thread.join()
+        pass
 
 
 def prepare_curses():
@@ -137,3 +164,6 @@ def run_diagnostics():
 
     d.draw()
     d.start()
+
+    import time
+    time.sleep(35)
