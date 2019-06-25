e9ae2ca6bcb3746bf90151e4ded71383fc014538
nickmflorin <nickmflorin@gmail.com>
2019-06-25 01:14:55 -0400

    Add termx as a submodule

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
 import os
 
 """
@@ -11,6 +12,9 @@ is not set, it uses `dev` by default.
 This means that if we want to use another settings file, we have to specify the
 ENV variable before any import from instattack is performed.
 """
+
+reqs = parse_requirements('./requirements.txt', session=False)
+install_requires = [str(ir.req) for ir in reqs]
 
 os.environ['INSTATTACK_SIMPLE_SETTINGS'] = 'dev'
 
@@ -32,8 +36,10 @@ setup(
     url='https://github.com/nickmflorin/instagram-attack',
     license='unlicensed',
     packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_dir={'instattack': 'instattack'},
     package_data={'instattack': ['templates/*']},
     include_package_data=True,
+    install_requires=install_requires,
     entry_points="""
         [console_scripts]
         instattack = instattack.main:instattack
